1163fb2a3165ea45a3b069e16994af5ac741106c
ShipPack <omamuli.emmanuel@gmail.com>
2024-10-03 10:38:31 +0100

    Updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
 }
 
 dependencies{
-    implementation("com.github.Omamuli-Emmanuel:transactpay_android:v0.4.6-stable")
-//    implementation(project(":transactpay_android"))
+//    implementation("com.github.Omamuli-Emmanuel:transactpay_android:v0.4.6-stable")
+    implementation(project(":transactpay_android"))
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
